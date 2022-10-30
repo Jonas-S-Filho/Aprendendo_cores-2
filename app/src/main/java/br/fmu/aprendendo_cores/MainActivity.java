38b5d484179de7087b1907be875f9dfebb4cf129
GustavoMaced <105135501+GustavoMaced@users.noreply.github.com>
2022-10-30 14:13:38 -0300

    Update MainActivity.java

--- app/src/main/java/br/fmu/aprendendo_cores/MainActivity.java
+++ app/src/main/java/br/fmu/aprendendo_cores/MainActivity.java
@@ -1,18 +1,36 @@
 package br.fmu.aprendendo_cores;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private android.content.Intent Intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView config = (ImageView) findViewById(R.id.imageView4);
+        config.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { config();
+            }
+
+        });
+
     }
+
+    public void config() {
+        Intent intent = new Intent(this, ConfigActivity.class);
+        startActivity(Intent);
+    }
+
 
     public void jogar(View view) {
         Intent intent = new Intent(this, SetDificuldadeActivity.class );
