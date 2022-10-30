b88cc57aa06ec539ca19c7e84df478f78c378d94
GustavoMaced <105135501+GustavoMaced@users.noreply.github.com>
2022-10-30 17:00:27 -0300

    Update ConfigActivity.java

--- app/src/main/java/br/fmu/aprendendo_cores/ConfigActivity.java
+++ app/src/main/java/br/fmu/aprendendo_cores/ConfigActivity.java
@@ -5,15 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.Switch;
 
 public class ConfigActivity extends AppCompatActivity {
     ImageView voltar;
+    Switch sound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config);
+
+        sound = findViewById(R.id.switch2);
+        sound.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (sound.isChecked()){
+                    // som será ativado
+                }else {
+                    // som será desativado
+                }
+            }
+        });
 
         voltar.findViewById(R.id.voltarconfig);
         voltar.setOnClickListener(new View.OnClickListener(){
